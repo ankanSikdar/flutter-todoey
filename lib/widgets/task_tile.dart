a552ffd75fb39b9467034fb9a0b2354ed78aab62
Ankan Sikdar <ankansikdar@gmail.com>
2020-08-18 21:23:18 +0530

    Added state to TaskTile

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -8,12 +15,33 @@ class TaskTile extends StatelessWidget {
         'Buy Milk',
         style: TextStyle(
           fontSize: 18.0,
+          decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
+      trailing: TaskCheckBox(
+        checkBoxState: isChecked,
+        onPressed: (bool changedState) {
+          setState(() {
+            isChecked = changedState;
+          });
+        },
       ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  TaskCheckBox({this.checkBoxState, this.onPressed});
+
+  final bool checkBoxState;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState,
+      onChanged: onPressed,
+      activeColor: Colors.lightBlueAccent,
     );
   }
 }
